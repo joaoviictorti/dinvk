7e8d73b727f04187bfed81eb5bf785ade6a3dd4a
joaoviictorti <joaovictorti08@gmail.com>
2025-09-25 20:38:17 -0300

    Update examples

--- examples/tampered_syscall/main.rs
+++ examples/tampered_syscall/main.rs
@@ -1,12 +1,9 @@
-use dinvk::{
-    data::HANDLE,
-    breakpoint::{
-        set_use_breakpoint, 
-        veh_handler
-    },
+use dinvk::breakpoint::{
+    set_use_breakpoint, 
+    veh_handler
 };
 use dinvk::{
-    NT_SUCCESS,
+    NT_SUCCESS, NtCurrentProcess,
     AddVectoredExceptionHandler, 
     NtAllocateVirtualMemory, 
     RemoveVectoredExceptionHandler,
@@ -15,12 +12,12 @@ use dinvk::{
 fn main() {
     // Enabling breakpoint hardware
     set_use_breakpoint(true);
-    let handle = AddVectoredExceptionHandler(0, Some(veh_handler));
+    let handle = AddVectoredExceptionHandler(1, Some(veh_handler));
 
     // Allocating memory and using breakpoint hardware
     let mut addr = std::ptr::null_mut();
     let mut size = 1 << 12;
-    let status = NtAllocateVirtualMemory(-1isize as HANDLE, &mut addr, 0, &mut size, 0x3000, 0x04);
+    let status = NtAllocateVirtualMemory(NtCurrentProcess(), &mut addr, 0, &mut size, 0x3000, 0x04);
     if !NT_SUCCESS(status) {
         eprintln!("[-] NtAllocateVirtualMemory Failed With Status: {}", status);
         return;
